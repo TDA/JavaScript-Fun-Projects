9969fb6d9a97194e5e23132e1fa5b6ec89b1b84c
Sai Pc <schand31@asu.edu>
2015-10-01 18:54:29 -0700

    Some js love

--- scripts/keypress.js
+++ scripts/keypress.js
@@ -8,9 +8,19 @@ $(document).ready(function () {
     var code = (e.keyCode ? e.keyCode : e.which);
 
     // (code > 33 && code <= 47) || (code > 57 && code <= 63) || code == 17
-    if (! ()) {
-      // if not an alphabet letter or number, reject the key, lets irritate them, switch up the key mappings, BURN.
-      console.log(e);
+    // if not an alphabet, letter or number, reject the key, lets irritate them, switch up the key mappings, BURN.
+
+    switch (true) {
+      // let these fall through, these are ok
+      // js only lang to allow this <3
+      case (code >= 65 && code <= 90):
+      case (code >= 48  && code <= 57):
+      case (code >= 97  && code <= 122):
+      case (code === 32):
+        console.log("legal");
+        break;
+      case
+
     }
     //console.log(e, "pressed");
   });
