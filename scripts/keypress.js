b97be24f161b4c3ff50cb6f72b081b364fb690c8
Sai Pc <schand31@asu.edu>
2015-10-01 18:33:36 -0700

    Initial commit

---
+++ scripts/keypress.js
@@ -0,0 +1,11 @@
+/**
+ * Created by schandramouli on 10/1/15.
+ */
+
+$(document).ready(function () {
+  var name = $('#name');
+  name.on("keypress", function (e) {
+
+    //console.log(e, "pressed");
+  });
+});
