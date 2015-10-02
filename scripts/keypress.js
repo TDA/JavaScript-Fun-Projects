1cf277947a16affa5aefee6cfd71405483c280ed
Sai Pc <schand31@asu.edu>
2015-10-01 18:42:23 -0700

    I am awesome

--- scripts/keypress.js
+++ scripts/keypress.js
@@ -5,7 +5,13 @@
 $(document).ready(function () {
   var name = $('#name');
   name.on("keypress", function (e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
 
+    // (code > 33 && code <= 47) || (code > 57 && code <= 63) || code == 17
+    if (! ()) {
+      // if not an alphabet letter or number, reject the key, lets irritate them, switch up the key mappings, BURN.
+      console.log(e);
+    }
     //console.log(e, "pressed");
   });
 });
