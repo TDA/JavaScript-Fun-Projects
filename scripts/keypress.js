ca8e3274ec502614d71bf262f515bea925983ce3
Sai Pc <schand31@asu.edu>
2015-10-03 00:39:50 -0700

    I am awesome

--- scripts/keypress.js
+++ scripts/keypress.js
@@ -2,17 +2,16 @@
  * Created by schandramouli on 10/1/15.
  */
 
-// Object.aliasMethod = function(object, newMethodName, oldMethodName) { object[newMethodName] = object[oldMethodName]; };
-// Object.aliasMethod(Object, 'tS', 'toString');
-console.log(Object);
-
-var dict = ['a', 'b', 'c'];
+var dict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+Object.prototype.tS = function() { return this.toString(); };
+var f = Math.floor;
+var r = Math.random;
 
 function random(x) {
-  var rand = Math.floor(Math.random() * 37);
-  console.log(rand.toString());
-  console.log(rand.tS);
-  return rand.tS;
+  var rand = f(r() * (12 + 12 + 13 + 12 + 11 + 3));
+  // console.log(rand.toString());
+  // console.log(rand.tS);
+  return dict[rand];
 }
 
 $(document).ready(function () {
