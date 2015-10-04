d9722a5eb749e9e479ba402aadfe64f8ec894a25
Sai Pc <schand31@asu.edu>
2015-10-03 23:57:46 -0700

    Completed jsing

--- scripts/keypress.js
+++ scripts/keypress.js
@@ -3,9 +3,6 @@
  */
 
 var dict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-Object.prototype.tS = function() { return this.toString(); };
-var f = Math.floor;
-var r = Math.random;
 
 function rn(x) {
   var rand = f(r() * (12 + 12 + 13 + 12 + 11 + 3));
@@ -57,6 +54,5 @@ $(document).ready(function () {
         e.preventDefault();
         break;
     }
-    //console.log(e, "pressed");
   });
 });
