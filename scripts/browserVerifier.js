b235f0aaab74aa1e8e98cbcb5a0abd7c1d6318b0
Sai Pc <schand31@asu.edu>
2015-10-27 15:44:21 -0700

    finished browser verifier yay

--- scripts/browserVerifier.js
+++ scripts/browserVerifier.js
@@ -2,25 +2,41 @@
 * Created by schandramouli on 10/27/15.
 */
 
-window.onload = function() {
+function checkUA() {
   var browserEngines = ['Trident', 'Gecko/', 'Chrome/', 'Safari/'];
   var browsers = ['Internet Explorer', 'Mozilla Firefox', 'Google Chrome', 'Safari', 'Not from a browser'];
   var index = 255;
-  var browserIndex = 0;
+  var browser;
+  var browserIndex = 4;
   if (navigator.userAgent.indexOf('Edge') == -1) {
-    console.log(navigator.userAgent);
+    //console.log(navigator.userAgent);
     for (var x = 0; x < browserEngines.length; x++) {
-      console.log(x, browserEngines);
+      //console.log(x, browserEngines);
       var newIndex = navigator.userAgent.indexOf(browserEngines[x]);
-      console.log(browserEngines[x], ' found at  ', newIndex);
+      //console.log(browserEngines[x], ' found at  ', newIndex);
       if (newIndex > -1 && index > newIndex) {
         index = newIndex;
         browserIndex = x;
-        console.log(x, newIndex);
+        //console.log(x, newIndex);
       }
     }
-    console.log(browsers[browserIndex]);
+    browser = (browsers[browserIndex]);
   } else {
-    console.log('Microsoft Edge, cool choice');
+    browser = ('Microsoft Edge, cool choice');
   }
-};
+
+  var osEngines = ['Mac', 'Windows'];
+  var os = ['Mac OS', 'Microsoft Windows', 'A flavor of Linux'];
+  var OSIndex = 2;
+  index = 255;
+  for (var y = 0; y < osEngines.length; y++) {
+    var newOSIndex = navigator.userAgent.indexOf(osEngines[y]);
+    //console.log(osEngines[y], ' found at  ', newOSIndex);
+    if (newOSIndex > -1 && index > newOSIndex) {
+      index = newOSIndex;
+      OSIndex = y;
+    }
+  }
+
+  return {'browser': browser, 'os': os[OSIndex]}
+}
