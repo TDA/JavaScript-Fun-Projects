b7e53d9d0c5998cc12f2d5edf48bc8d1f0d80dca
Sai Pc <schand31@asu.edu>
2015-11-13 09:34:38 -0700

    trying out stuff for sreekanth

--- scripts/hasher.js
+++ scripts/hasher.js
@@ -7,15 +7,15 @@ function hashCode(str) {
   if (str.length == 0) return hash;
   for (i = 0; i < str.length; i++) {
     char = str.charCodeAt(i);
-    //hash = ((hash << 5) - hash) + char; /* hash * 31 + char */
+    //hash = ((hash << 5) - hash) + char; /* is the same as hash * 31 + char */
     hash = hash * 31 + char;
-    //hash = hash & hash; // Convert to 32bit integer
+    //hash = hash & hash; // this has destructive nature, and we can avoid it for local testing
     console.log(hash);
   }
   return hash;
 }
 
-console.log(hashCode("Dragon123@"));
+hashCode("Dragon123@");
 console.log("------------------------");
 //var string = "Dragon123@";
 //var sum = 0;
@@ -27,10 +27,20 @@ function reverseHashCode(str) {
   var hash = str;
   var length = 0;
   if (str.length == 0) return 0;
-  else length = str.length;
-
-  hash = hash % (31 * length);
+  else length = str.toString().length;
+  var arrayValues = [];
+  for (var i = 0; i < length - 1 && hash > 31; i++) {
+    hash = hash / (31);
+    arrayValues.push(hash);
+    console.log(hash);
+  }
+  firstLetter = Math.floor(Math.floor(arrayValues[arrayValues.length - 2]) - arrayValues[arrayValues.length - 1]);
+  console.log(String.fromCharCode(firstLetter));
+  secondLetter = Math.floor(arrayValues[arrayValues.length - 3]) - Math.ceil(arrayValues[arrayValues.length - 1]) - firstLetter * 31;
+  console.log(String.fromCharCode(secondLetter));
+  thirdLetter = Math.floor(arrayValues[arrayValues.length - 4]) - Math.ceil(arrayValues[arrayValues.length - 1]) - Math.floor(arrayValues[arrayValues.length - 3] - Math.ceil(arrayValues[arrayValues.length - 1])) * 31;
+  console.log(String.fromCharCode(thirdLetter));
   return hash;
 }
 
-console.log(reverseHashCode("-2910953215"));
+reverseHashCode("1897887302574337");
