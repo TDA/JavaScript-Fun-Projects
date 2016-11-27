1c98424bf3c74aaac8c322565f4c87854ac95641
Sai Chandramouli <schand31@asu.edu>
2016-11-26 22:23:49 -0800

    made the retrieve queries more generic

--- parse_query_test.js
+++ parse_query_test.js
@@ -2,8 +2,15 @@ var Parse = require('parse/node');
 Parse.initialize("SAI_TEST");
 Parse.serverURL = 'http://localhost:1337/parse'
 
-var GameScore = Parse.Object.extend("Survey");
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+var tableName = process.argv[2];
+
+var GameScore = Parse.Object.extend(tableName);
 var query = new Parse.Query(GameScore);
+
+var params = process.argv.slice(3)
 
 query.find({
   success: function(results) {
@@ -11,7 +18,11 @@ query.find({
     // Do something with the returned Parse.Object values
     for (var i = 0; i < results.length; i++) {
       var object = results[i];
-      console.log(object.id + ' - ' + object.get('name') + ' - ' + object.get('mail') + ' - ' + object.get('playerName'));
+      var consoleString = '';
+      params.forEach((val, index) => {
+        consoleString += ' - ' + object.get(val);
+      });
+      console.log(object.id + consoleString);
     }
   },
   error: function(error) {
